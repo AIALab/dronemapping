babfb77372790b310a6b54f04cea27e7921b881a
AIA System Laboratory <39199907+AIALab@users.noreply.github.com>
2018-05-21 12:46:19 +0530

    Delete admin.py

--- nodeodm/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from guardian.admin import GuardedModelAdmin
-
-from .models import ProcessingNode
-
-class ProcessingNodeAdmin(GuardedModelAdmin):
-    fields = ('hostname', 'port')
-
-admin.site.register(ProcessingNode, ProcessingNodeAdmin)
